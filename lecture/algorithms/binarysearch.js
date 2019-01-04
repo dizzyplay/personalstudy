f60e70b415d570c474bc59e563120e4682a542aa
dizzyplay <vovoboss@gmail.com>
2019-01-05 02:20:59 +0900

    ok

--- lecture/algorithms/binarysearch.js
+++ lecture/algorithms/binarysearch.js
@@ -1,14 +1,15 @@
 function bt(arr, key) {
-  let mid = Math.floor(arr.length / 2);
-  let midValue = arr[mid];
+  let midIndex = Math.floor(arr.length / 2);
+  let midIndexValue = arr[midIndex];
 
-  if (midValue === key) {
+  if (midIndexValue === key) {
     return key;
-  } else if (midValue > key && arr.length > 1) {
-    return bt(arr.slice(0, mid), key);
-  } else if (midValue < key && arr.length > 1) {
-    return bt(arr.slice(mid), key);
+  } else if (midIndexValue > key && arr.length > 1) {
+    return bt(arr.slice(0, midIndex), key);
+  } else if (midIndexValue < key && arr.length > 1) {
+    return bt(arr.slice(midIndex, arr.length), key);
   } else return false;
 }
 
-console.log(bt([5, 7, 12, 16, 36, 39, 42, 56, 71], 56));
+console.log(bt([5, 7, 12, 16, 36, 39, 42, 56, 71], 12));
+console.log(bt([5, 7, 12, 16, 36, 39, 42, 56, 71], -1));
