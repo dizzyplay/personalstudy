838486c9c7a2e5ee1862b41d55ce0437d690d583
dizzyplay <vovoboss@gmail.com>
2019-01-19 10:25:53 +0900

    ok

---
+++ c/headertest/foo.h
@@ -0,0 +1,4 @@
+#ifndef FOO_H_
+#define FOO_H_ 
+int foo(int x);
+#endif /* ifndef FOO_H_ */
---
+++ c/headertest/main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include "foo.h"
+
+int main(void){
+  int y=foo(3);
+  printf("%d",y);
+  return 0;
+}
