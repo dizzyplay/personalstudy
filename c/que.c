73d7a9811c6782e05e896b8667044420e5aaca9a
dizzyplay <vovoboss@gmail.com>
2019-05-27 04:54:05 +0900

    c programming

---
+++ c/que.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct qNode{
+  char *data;
+  struct qNode *next;
+}node;
+
+node* push(node *parent ,char *v){
+  node *n = malloc(sizeof(node));
+  n->data = v;
+  parent->next = n;
+
+  return n;
+}
+
+int main(){
+  node *first = malloc(sizeof(node));
+  node *second = malloc(sizeof(node));
+  first->data="test";
+  second = push(first,"second");
+  printf("%s",second->data);
+  return 0;
+}
