90b2d7f6814178dfbfded7218283a2f33f0cfa97
dizzyplay <vovoboss@gmail.com>
2019-06-03 01:07:07 +0900

    mv cpp

---
+++ cpp/hello.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+int main(){
+  int user_input;
+  cout << "input" << endl;
+  cin >> user_input;
+  cout << user_input << endl;
+  for(int i=0; i<10; i++){
+    cout << "hello" << i << endl;
+  }
+
+  return 0;
+}
