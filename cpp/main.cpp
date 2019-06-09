cfc933eb6244912b8aa999bbd8f7cf8db2b2cfc1
dizzyplay <vovoboss@gmail.com>
2019-06-09 18:00:47 +0900

    ok

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -1,18 +1,17 @@
 #include <iostream>
-#include <string>
-#include <stdio.h>
 
 using namespace std;
 
 int main(){
-  string s,str;
-  int n;
-  cin >> s;
-  n= s.length();
-  for(int i=1; i<n+1; i++){
-    cout << s[i-1];
-    if(i%10==0){
-      cout << "\n";
+  int n,x;
+  cin >> n >> x;
+  int a[n];
+  for(int i=0; i<n; i++){
+    cin >>a[i];
+  }
+  for(int i=0; i<n; i++){
+    if(a[i] < x){
+      cout << a[i] << " ";
     }
   }
   return 0;
