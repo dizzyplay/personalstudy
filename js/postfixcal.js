7fe0a95caec8879225a85ac83c7b4e946da4224c
dizzyplay <vovoboss@gmail.com>
2019-07-24 15:41:29 +0900

    ok

--- js/postfixcal.js
+++ js/postfixcal.js
@@ -1,59 +1,26 @@
-nArr = '1234567890';
 Array.prototype.peek = function() {
   return this[this.length - 1];
 };
 
-function tokenToList(tokens) {
-  token_list = [];
-  new_list = [];
-  token_list = tokens.split('');
-  temp = '';
-  for (t of token_list) {
-    if (nArr.indexOf(t) != -1) {
-      temp += t;
+function postfixCal(arr) {
+  const token = ['+', '-', '*', '/'];
+  const stack = [];
+  const temp = [];
+  while (arr.length > 0) {
+    if (token.indexOf(arr[0]) != -1) {
+      t = arr.shift();
+      second = stack.pop();
+      first = stack.pop();
+      exp = first + t + second;
+      stack.push(eval(exp));
     } else {
-      if (temp != 0) new_list.push(Number(temp));
-      temp = '';
-      new_list.push(t);
+      stack.push(arr.shift());
     }
   }
-  if (temp) new_list.push(Number(temp));
-  return new_list;
+  return stack[0];
 }
 
-function convertToPostfix(arr) {
-  let stack = [];
-  let result = [];
-  const tokens = {'(': 0, '-': 1, '+': 1, '*': 2, '/': 2};
-  for (t of arr) {
-    if (typeof t != 'number') {
-      if (t == '(') stack.push(t);
-      else if (tokens[stack.peek()] >= tokens[t]) {
-        result.push(stack.pop());
-        stack.push(t);
-      } else if (t == ')') {
-        while (stack.peek() != '(') {
-          result.push(stack.pop());
-        }
-        stack.pop();
-      } else {
-        stack.push(t);
-      }
-    } else {
-      result.push(t);
-    }
-  }
-  while (stack.length) {
-    result.push(stack.pop());
-  }
-  return result;
-}
+console.log(postfixCal(['2', '3', '4', '+', '*']));
+console.log(postfixCal(['2', '3', '*', '4', '+']));
 
-function sol(exp) {
-  expArr = tokenToList(exp);
-  postfix = convertToPostfix(expArr);
-  console.log(postfix);
-}
-
-sol('(10+20)*(10+20)');
-sol('10+10+10');
+// python 에서 pop(0)은 js에 없다 python:  pop(0) == js: Array.shift()
